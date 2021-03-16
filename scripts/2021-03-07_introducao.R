06432e63a07893ee1a2a4a599247069f26b9574c
maykongpedro <maykonglaffite@gmail.com>
2021-03-16 14:44:37 -0300

    add vetores para responder aos questionamentos

--- scripts/2021-03-07_introducao.R
+++ scripts/2021-03-07_introducao.R
@@ -13,7 +13,7 @@ library(magrittr)
 
 
 # Ler arquivos ----------------------------------------------------------
-
+source("scripts/2021-03-14_analise_covid.R", encoding = "UTF-8")
 
 
 # Ver a estrutura do projeto no console: quais arquivos estão presentes?
@@ -23,23 +23,38 @@ fs::dir_tree()
 
 
 # Qual foi o total de óbitos por covid até o momento? ---------------------
-
+mensagem_obitos_br
 
 
 # Qual foi o total de óbitos por covid em 2020? ---------------------------
-
+covid_mensagem_obitos_br_2020
 
 
 # Qual foi o total de óbitos por SARG em 2020? ----------------------------
-
+sarg_mensagem_obitos_br_2020
 
 
 # Qual foi o total de óbitos por SARG nos últimos anos? -------------------
+for (ano in anos) {
+  
+  obitos <- sarg_obitos_por_ano_df[sarg_obitos_por_ano_df$ano == ano, "obitos"]
+  
+  mensagem_sarg_obitos_por_ano <- 
+    paste0(
+      "A quantidade de óbitos por SARG em ",
+      ano,
+      " foi de ",
+      obitos)
+  
+  print(mensagem_sarg_obitos_por_ano)
+  
+}
+sarg_obitos_por_ano_df
 
 
 
 # Quais são as estatísticas básicas dos óbitos de SARG antes de 2020 ------
-
+estatisticas_obitos_sarg_ate_2019
 
 
 # Quantos óbitos em 2020 ultrapassam a média? -----------------------------
